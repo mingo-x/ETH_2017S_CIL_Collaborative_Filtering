4816ea15308171591b9bdf8443287beed581abce
Yanping <rea.s.xyp@gmail.com>
2017-06-21 14:52:54 +0200

    add missing data filling

--- code/SecondDraft.py
+++ code/SecondDraft.py
@@ -2,8 +2,8 @@
 import numpy as np
 import csv
 from sys import argv
+import time
 
-print('hi')
 # read in training data
 data = np.zeros((10000,1000))
 print('start reading data')
@@ -21,15 +21,27 @@ for row in csvReader:
 	data[i,j] = val
 print('finish reading data')
 
+# fill in missing data
+print('start filling in missing data')
+for i in range(1000):
+	missing = data[:,i]==0
+	known = missing==False
+	mean_of_known = np.mean(data[known,i])
+	print("mean of col ",i+1,mean_of_known)
+	data[missing,i] = mean_of_known
+print('finish filling in missing data')
+
 # SVD
 print('start SVD')
+startTime = time.time()
 U, s, Vt = np.linalg.svd(data, full_matrices=True)
-print('finish SVD', U.shape, s.shape, Vt.shape)
+endTime = time.time()
+print('finish SVD', U.shape, s.shape, Vt.shape, int(endTime-startTime), 's')
 S = np.zeros((10000, 1000))
 S[:1000, :1000] = np.diag(s)
 # whether two arrays are element-wise equal within a tolerance
 A = U.dot(S).dot(Vt)
-print('close? ', np.allclose(data, U.dot(S).dot(Vt)))
+print('close?', np.allclose(data, U.dot(S).dot(Vt)))
 # k = 2
 # Sk = S.copy()
 # Sk[k:, k:] = 0
