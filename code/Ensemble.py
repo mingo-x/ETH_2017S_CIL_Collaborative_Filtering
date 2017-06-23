d9dc754c4122a8ea1e18725461dde1eeaea86d94
Yanping <rea.s.xyp@gmail.com>
2017-06-23 05:42:51 +0200

    ensemble of predictors

--- code/Ensemble.py
+++ code/Ensemble.py
@@ -1,10 +1,56 @@
 from sklearn import linear_model
 import SVD
+import numpy as np
+import Globals
 
 def ensemble(data):
 	known = data!=0
-	target = data[known]
+	nObs = np.count_nonzero(data)
+	target = np.reshape(data[known],(nObs,1))
 
-	A_
+	Basic1_A = np.load('./log/Basic1_A.npy')
+	Basic2_A = np.load('./log/Basic2_A.npy')
+	Basic3_A = np.load('./log/Basic3_A.npy')
+	Basic4_A = np.load('./log/Basic4_A.npy')
+	Basic5_A = np.load('./log/Basic5_A.npy')
+	Basic6_A = np.load('./log/Basic6_A.npy')
+	KMeans_A = np.load('./log/Kmeans_A_combi.npy')
+	PSVD_A = np.load('./log/PSVD_A20_clip.npy')
+	RSVD_A = np.load('./log/RSVD_A_20_combi.npy')
+	RSVD2_A = np.load('./log/RSVD2_A_20_combi.npy')
+
+	train = np.append([Basic1_A[known]],[Basic2_A[known]],axis=0)
+	train = np.append(train,[Basic3_A[known]],axis=0)
+	train = np.append(train,[Basic4_A[known]],axis=0)
+	train = np.append(train,[Basic5_A[known]],axis=0)
+	train = np.append(train,[Basic6_A[known]],axis=0)
+	train = np.append(train,[KMeans_A[known]],axis=0)
+	train = np.append(train,[PSVD_A[known]],axis=0)
+	train = np.append(train,[RSVD_A[known]],axis=0)
+	train = np.append(train,[RSVD2_A[known]],axis=0)
+	train = train.T
+
+	regr = linear_model.LinearRegression()
+	regr.fit(train, target)
+
+	test = np.append([Basic1_A.flatten()],[Basic2_A.flatten()],axis=0)
+	test = np.append(test,[Basic3_A.flatten()],axis=0)
+	test = np.append(test,[Basic4_A.flatten()],axis=0)
+	test = np.append(test,[Basic5_A.flatten()],axis=0)
+	test = np.append(test,[Basic6_A.flatten()],axis=0)
+	test = np.append(test,[KMeans_A.flatten()],axis=0)
+	test = np.append(test,[PSVD_A.flatten()],axis=0)
+	test = np.append(test,[RSVD_A.flatten()],axis=0)
+	test = np.append(test,[RSVD2_A.flatten()],axis=0)
+	test = test.T
+
+	A = regr.predict(test)
+	A = np.reshape(pres,(Globals.nUsers,Globals.nItems))
+	return A
 
 if __name__ == "__main__":
+	Initialization.initialization()
+	data = Initialization.readInData('./data/data_train.csv')
+	A = ensemble(data)
+	np.save('./log/Ensemble_A.npy',A)
+	SVD.writeOutData(A)
