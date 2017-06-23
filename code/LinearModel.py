228940ea030958e76ffffb1ca20c3e3955cc976c
Yanping <rea.s.xyp@gmail.com>
2017-06-23 10:07:39 +0200

    change stop condition

--- code/LinearModel.py
+++ code/LinearModel.py
@@ -30,7 +30,7 @@ def gradientDescent(data,train,testMask):
 	t = 0
 	A = np.empty((Globals.nUsers,Globals.nItems))
 	startTime = time.time()
-	while prev-curr > 1e-10:
+	while prev-curr > 1e-8:
 		w *= 1-lamb
 		for i in range(Globals.nUsers):
 			yp = m.copy()
