ae88dc72e61777c9c5eb77a7d78be51993a513d7
Yanping <rea.s.xyp@gmail.com>
2017-06-28 14:09:07 +0200

    fix

--- code/Analysis.py
+++ code/Analysis.py
@@ -6,7 +6,7 @@ import csv
 if __name__ == '__main__':
 	Initialization.initialization()
 	data = Initialization.readInData('./data/data_train.csv')
-	c = np.count_nonzero(data,axis=0)
 	csvWriter = csv.writer(open('./log/itemRatingCount.csv','w',newline=''))
 	for i in range(Globals.nItems):
-		csvWriter.writerow([c[i]])
+		c = np.count_nonzero(data[:,i])
+		csvWriter.writerow([c])
