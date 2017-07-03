b218f065b1d3d729e0f3ebd11554d21c35dc3b2f
Yanping <rea.s.xyp@gmail.com>
2017-07-03 05:47:57 +0200

    fix

--- code/UserBased.py
+++ code/UserBased.py
@@ -64,6 +64,8 @@ def predict(u,j,known,score,data):
 		term += np.abs(score[u,i])
 	pred /= term
 	pred += mu[u]
+	if np.isnan(pred):
+		print(pred,term,peers.shape)
 		
 def output(test,known,score,data,vali):
 	print('start predicting')
